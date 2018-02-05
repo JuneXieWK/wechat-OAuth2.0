e48da36e44179592f5cb0c46aadbf47400725f54
June Xie <june.xie@wk.com>
2018-02-05 18:24:38 +0800

    test

--- demo/testuserinfo.php
+++ demo/testuserinfo.php
@@ -19,13 +19,13 @@ $oauth2->init($APPID, $SECRET,$REDIRECT_URL);
 if(empty($code)){		
 	$oauth2->get_code_by_authorize($state);//获取code，会重定向到当前页。若需传参，使用$state变量传参。
 }
-$data=$oauth2->get_userinfo_by_authorize();
+// $data=$oauth2->get_userinfo_by_authorize();
 
-echo '</br>welcome test!';
-echo '</br>nickname: '.$data['nickname'];
-echo '</br>headimgurl: '.$data['headimgurl'];
-echo '</br>dataRaw: '.$data['dataRaw'];
-echo '</br>nickname1: '.$data['nickname1'];
+// echo '</br>welcome test!';
+// echo '</br>nickname: '.$data['nickname'];
+// echo '</br>headimgurl: '.$data['headimgurl'];
+// echo '</br>dataRaw: '.$data['dataRaw'];
+// echo '</br>nickname1: '.$data['nickname1'];
 
 
 
