3a8916aa15de5b75cc0d3638001425a8a9238f16
June Xie <june.xie@wk.com>
2018-02-05 17:41:49 +0800

    test

--- demo/oauth2.php
+++ demo/oauth2.php
@@ -110,13 +110,13 @@ class oauth2{
 		$SECRET=$this->SECRET;
 		$code=$this->Code;
 			
-		$url="https://api.weixin.qq.com/sns/oauth2/access_token?appid=$APPID&secret=$SECRET&code=$code&grant_type=authorization_code";
+		$url="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$APPID&secret=$SECRET";
 		$content=file_get_contents($url);
 		$o=json_decode($content,true);
 		$openid=$o['openid'];
 		$access_token=$o['access_token'];
 
-		 $url = "https://api.weixin.qq.com/cgi-bin/message/send?access_token=".$access_token;
+		 $url = "https://api.weixin.qq.com/cgi-bin/message/send?access_token=$access_token";
 		 $res = $this->http_request($url, $data);
 		 return json_decode($res, true);
 	 }
