5526356db64ca8c78920d50dbe014d97a6a66e01
June Xie <june.xie@wk.com>
2018-02-05 13:51:13 +0800

    fill wx account info

--- demo/testuserinfo.php
+++ demo/testuserinfo.php
@@ -9,9 +9,9 @@ include './oauth2.php';
 $code=$_GET['code'];//code 微信接口参数(必须)
 $state=$_GET['state'];//state微信接口参数(不需传参则不用)；若传参可考虑规则： 'act'.'arg1'.'add'.'arg2'
 
-$APPID='';
-$SECRET='';
-$REDIRECT_URL='';//当前页面地址
+$APPID='wx922bb25878a01bea';
+$SECRET='124b06ab6e1bb82b2d46b24e56fee1f3';
+$REDIRECT_URL='http://wkly.com/dev/react/wechat-OAuth2.0/demo/testuserinfo.php';//当前页面地址
 
 $oauth2=new oauth2();
 $oauth2->init($APPID, $SECRET,$REDIRECT_URL);
