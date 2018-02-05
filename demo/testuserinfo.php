76738f6081165e62b6b456bc17035d434344b65f
June Xie <june.xie@wk.com>
2018-02-05 17:59:53 +0800

    test

--- demo/testuserinfo.php
+++ demo/testuserinfo.php
@@ -19,26 +19,26 @@ $oauth2->init($APPID, $SECRET,$REDIRECT_URL);
 if(empty($code)){		
 	$oauth2->get_code_by_authorize($state);//获取code，会重定向到当前页。若需传参，使用$state变量传参。
 }
-// $data=$oauth2->get_userinfo_by_authorize();
+$data=$oauth2->get_userinfo_by_authorize();
 
-// echo '</br>welcome test!';
-// echo '</br>nickname: '.$data['nickname'];
-// echo '</br>headimgurl: '.$data['headimgurl'];
-// echo '</br>dataRaw: '.$data['dataRaw'];
-// echo '</br>nickname1: '.$data['nickname1'];
+echo '</br>welcome test!';
+echo '</br>nickname: '.$data['nickname'];
+echo '</br>headimgurl: '.$data['headimgurl'];
+echo '</br>dataRaw: '.$data['dataRaw'];
+echo '</br>nickname1: '.$data['nickname1'];
 
-$template = array('touser'=>"测试号的关注者的openId",
-'template_id'=> "1zFGoGtcuC9s81ph8QWClXhBkGkjCP2qZVojVfN9QCs",
-'url'=>"http://www.baidu.com",
-'topcolor'=>"#7B68EE",
-'data'=>array(
-	'first' => array('value'=>urlencode("你好"), 'color'=>'#743A3A'),
-	'keyword1'=>array('value'=>urlencode("XXXXX"), 'color'=>'#FF0000'),
-	'keyword2'=>array('value'=>urlencode("XXXXX"), 'color'=>'#FF0000'),
-	'keyword3'=>array('value'=>urlencode("XXXXX"), 'color'=>'#FF0000'),
-	'remark'=>array('value'=>urlencode("XXXXX"), 'color'=>'#FF0000'),
-));
+// $template = array('touser'=>"测试号的关注者的openId",
+// 'template_id'=> "1zFGoGtcuC9s81ph8QWClXhBkGkjCP2qZVojVfN9QCs",
+// 'url'=>"http://www.baidu.com",
+// 'topcolor'=>"#7B68EE",
+// 'data'=>array(
+// 	'first' => array('value'=>urlencode("你好"), 'color'=>'#743A3A'),
+// 	'keyword1'=>array('value'=>urlencode("XXXXX"), 'color'=>'#FF0000'),
+// 	'keyword2'=>array('value'=>urlencode("XXXXX"), 'color'=>'#FF0000'),
+// 	'keyword3'=>array('value'=>urlencode("XXXXX"), 'color'=>'#FF0000'),
+// 	'remark'=>array('value'=>urlencode("XXXXX"), 'color'=>'#FF0000'),
+// ));
 
-var_dump($oauth2->send_template_message(urldecode(json_encode($template))));
+// var_dump($oauth2->send_template_message(urldecode(json_encode($template))));
 
 ?>
